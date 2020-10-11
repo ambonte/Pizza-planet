1503645fd145e1a5155a9542540fb4a64f35a402
Bonte Emma BANA <bbonteemma@gmail.com>
2020-10-11 19:35:39 +0200

    adding toggle effetcts

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
 
-function getSizeValue() {
+function pizzaSize() {
     var selectedValue = document.getElementById("size").value;
     return parseInt(selectedValue);
 }
-function getCrust() {
+function pizzaCrust() {
     var selectedCrust = document.getElementById("crust").value;
     return parseInt(selectedCrust);
 }
-function getToppings() {
+function pizzaToppings() {
     var selectedToppings = document.getElementById("toppings").value;
     return parseInt(selectedToppings);
 }
-function getQuantity() {
+function pizzaQuantity() {
     var selectedQuantity = document.getElementById("quan").value;
     return parseInt(selectedQuantity);
 }
-function getTotalAmount() {
-    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * getQuantity();
-    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is  "  +  (totalAmount)  +  ""  + "Rwf." +" Thank you working with us!");
+function pizzaTotalAmount() {
+    var totalAmount = (pizzaSize() + pizzaCrust() + pizzaToppings()) * pizzaQuantity();
+    alert("You have Ordered" + " " + pizzaQuantity("")  +  " pizza."  +  " "  +  " The Total Amount is  "  +  (totalAmount)  +  " "  + "Rwf." +" Thank you working with us!");
     prompt("Please enter your location")
     alert("You delivery fee is 500 Rwf!")
     alert("You order will be delivered shortly!!")
-    alert("Continue working with us, if you have any problem, you may contact us!! .")
+    alert("Continue working with us, if you have any problem, you may contact us!! ")
 }
 
-
+/******TOGGLE**********************************************************/
+$(document).ready(function () {
+    $(".tapp, .table").click(function () {
+      $("#tapp").toggle();
+      $(".table").toggle();
+    });
+  });
+  
+  
